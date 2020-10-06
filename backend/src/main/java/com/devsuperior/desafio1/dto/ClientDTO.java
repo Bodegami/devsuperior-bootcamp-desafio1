200e3b1a0bc2c6c75d64562478deb1a6721387df
Renato <rena.f.oliveira@gmail.com>
2020-10-06 13:52:40 -0300

    Class Client, database H2

---
+++ backend/src/main/java/com/devsuperior/desafio1/dto/ClientDTO.java
@@ -0,0 +1,17 @@
+package com.devsuperior.desafio1.dto;
+
+import java.time.Instant;
+
+public class ClientDTO {
+	
+	private Long id;
+	private String name;
+	private String cpf;
+	private Double income;
+	
+	
+	private Instant birthDate;
+	
+	private Integer children;
+
+}
---
+++ backend/src/main/java/com/devsuperior/desafio1/services/ClientService.java
@@ -0,0 +1,23 @@
+package com.devsuperior.desafio1.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.desafio1.entities.Client;
+import com.devsuperior.desafio1.repositories.ClientRepository;
+
+@Service
+public class ClientService {
+	
+	@Autowired
+	private ClientRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<Client> findAll() {
+		return repository.findAll();
+	}
+ 	
+}
